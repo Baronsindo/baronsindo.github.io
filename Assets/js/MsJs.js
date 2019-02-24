7f62f9b0ff7c9794a3f9a42c060fc62aa9d26626
Baronsindo <baronsindo@gmail.com>
2019-02-24 19:29:50 +0100

    My Static WebSite
    With HTML/JS/CSS/BOOTSTRAP

--- Assets/js/MsJs.js
+++ Assets/js/MsJs.js
@@ -1,47 +1,55 @@
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-  });
-
-  // Slide in elements on scroll
-  $(window).scroll(function() {
-	      var value = $(this).scrollTop();
-    if ( value > 350 ){
-        $(".navbar-nav").css("margin-top", "0px;");
-        $(".navbar-brand").css("display", "none");
-
-	}
-    else{
-        $(".navbar-nav li a").css("margin-top", "20px;");
-        $(".navbar-brand").css("display", "inline");
-
-	}
+// #region Loding blog posts
 
 
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
+$.ajax({
+    url: 'Assets/MyPosts.json',
+    type: "get",
+    dataType: "json",
 
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
+    success: function (data) {
+        drawTable(data);
+    }
+});
+
+function drawTable(data) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].Tag == "Codes") {
+            drawRow(data[i], i, "#fragCode");
+        } else if (data[i].Tag == "Blog") {
+            drawRow(data[i], i, "#fragBlog");
         }
-    });
-  });
+    }
+
+    $(".slideanim").addClass("slide");
+
+
+}
+
+
+function drawRow(rowData, i, frag) {
+
+    console.log(frag);
+    var row = $("<div />")
+    $(frag).append(row);
+    row.append($("<div class=\"col-md-4 col-sm-12 blg slideanim\">" +
+        "<center> <img src=\"" + rowData.Image + "\" class=\"img-rounded blogimg\" data-toggle=\"modal\" data-target=\"#Mymodal" + i + "\"></center>" +
+        "<h4 class=\"Title\"> <center>" + rowData.Title + " </h4>" +
+        "<div class=\"modal fade\" id=\"Mymodal" + i + "\" aria-hidden=\"true\"><div class=\"modal-dialog\" role=\"document\"><div class=\"modal-content\"><div class=\"modal-body\">" +
+        "<h2><center>" + rowData.Title + "</center></h2>" +
+        "<p>" + rowData.Content + " </center><br> <a href=\"#Mymodal" + i + "\">Share</a> </p>" +
+        "</div></div></div></div></div>"
+    ));
+}
+
+// #endregion
+
+$(document).ready(function () {
+
+    var get_modal = window.location.href.split('#');
+    var mo = get_modal[1].toString();
+
+    if ( mo != 'fragCode' && mo != 'fragBlog') {
+        console.log("Hash is : "+get_modal[1]);
+        $("#" + get_modal[1]).modal('show');
+    }
 })
